310559713ac615f8472afbf2fda59d341c922420
kiruba132 <kirubanithi.sornamoorthy@my.jcu.edu.au>
2025-06-07 14:29:30 +1000

    Add randoms.py file and complete questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+"""
+What did you see on line 1?
+smallest number = 5, largest number = 20
+What did you see on line 2?
+smallest number = 3, largest number = 9
+it can not produce a 4 as it began at 3 and increases by 2
+What did you see on line 3?
+smallest number = 2.5, largest number = 5.5
+"""
+import random
+print(random.randint(1,101)) # line 3
+
+

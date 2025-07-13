a132ecc1c4817a19bc037ec545634874b8c0507b
kiruba132 <kirubanithi.sornamoorthy@my.jcu.edu.au>
2025-07-13 13:22:01 +1000

    Complete required modifications for walkthrough example

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,31 +1,23 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""
+CP1404/CP5632 Practical - Client code to use the Car class.
+Note that the import has a folder (module) in it.
+This is why we name our folders with no spaces or capitals, as valid module names.
+"""
+
+from prac_06.car import Car
 
 
-class Car:
-    """Represent a Car object."""
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car("MyCar", 180)
+    my_car.drive(30)
+    print(f"Car has fuel: {my_car.fuel}")
+    print(my_car)
 
-    def __init__(self, fuel=0):
-        """Initialise a Car instance.
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo)
 
-        fuel: float, one unit of fuel drives one kilometre
-        """
-        self.fuel = fuel
-        self._odometer = 0
-
-    def add_fuel(self, amount):
-        """Add amount to the car's fuel."""
-        self.fuel += amount
-
-    def drive(self, distance):
-        """Drive the car a given distance.
-
-        Drive given distance if car has enough fuel
-        or drive until fuel runs out return the distance actually driven.
-        """
-        if distance > self.fuel:
-            distance = self.fuel
-            self.fuel = 0
-        else:
-            self.fuel -= distance
-        self._odometer += distance
-        return distance
+main()

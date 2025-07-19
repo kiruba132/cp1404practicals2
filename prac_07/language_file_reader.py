53ac041ba689a07d11b99abaa4dc52a5169a8840
kiruba132 <kirubanithi.sornamoorthy@my.jcu.edu.au>
2025-07-20 00:44:08 +1000

    Add pointer_arithmetic attribute to ProgrammingLanguage class

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -13,31 +13,20 @@ from programming_language import ProgrammingLanguage
 def main():
     """Read file of programming language details, save as objects, display."""
     languages = []
-    # Open the file for reading
-    in_file = open('languages.csv', 'r')
-    # File format is like: Language,Typing,Reflection,Year
-    # 'Consume' the first line (header) - we don't need its contents
-    in_file.readline()
-    # All other lines are language data
-    for line in in_file:
-        # print(repr(line))  # debugging
-        # Strip newline from end and split it into parts (CSV)
-        parts = line.strip().split(',')
-        # print(parts)  # debugging
-        # Reflection is stored as a string (Yes/No) and we want a Boolean
-        reflection = parts[2] == "Yes"
-        # Construct a ProgrammingLanguage object using the elements
-        # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
-        # Add the language we've just constructed to the list
-        languages.append(language)
-    # Close the file as soon as we've finished reading it
-    in_file.close()
+    with open('languages.csv', 'r') as in_file:
+        in_file.readline()  # skip header
+        for line in in_file:
+            parts = line.strip().split(',')
+            name = parts[0]
+            typing = parts[1]
+            reflection = parts[2] == "Yes"
+            year = int(parts[3])
+            pointer_arithmetic = parts[4] == "Yes"
+            language = ProgrammingLanguage(name, typing, reflection, year, pointer_arithmetic)
+            languages.append(language)
 
-    # Loop through and display all languages (using their str method)
     for language in languages:
         print(language)
-
 
 main()
 
